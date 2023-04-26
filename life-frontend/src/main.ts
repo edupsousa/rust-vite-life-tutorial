e49dd6a836a8719a5cb50c2661c8440524c5e6d5
Eduardo P de Sousa <edupsousa@gmail.com>
2023-04-25 22:23:40 -0300

    game of life using pre element

--- life-frontend/src/main.ts
+++ life-frontend/src/main.ts
@@ -1,4 +1,17 @@
 import "./style.css";
-import * as life from "life-wasm";
+import { Universe } from "life-wasm";
 
-life.greet();
+const appElement = document.getElementById("app");
+if (appElement === null) {
+  throw new Error("failed to get app element");
+}
+const universe = Universe.new();
+
+const renderLoop = () => {
+  appElement.textContent = universe.render();
+  universe.tick();
+
+  requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
